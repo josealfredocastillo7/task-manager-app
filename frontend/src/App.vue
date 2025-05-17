a1cb5b911386364f47a6efaa6bc20b3ebe1cb9f5
Jose Alfredo <jose.castillo@2becommerce.com>
2025-05-17 14:14:20 -0500

    feat: update app title and customize footer with personal information; implement theme toggle in layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-spacer />
-      <v-btn icon @click="toggleTheme">
-        <v-icon>{{ isDarkTheme ? 'mdi-weather-sunny' : 'mdi-moon-waxing-crescent' }}</v-icon>
-      </v-btn>
-    </v-app-bar>
     <router-view />
   </v-app>
 </template>
 
 <script setup>
-import { useTheme } from 'vuetify'
-
-const theme = useTheme()
-const isDarkTheme = computed(() => theme.global.name.value === 'dark')
-
-const toggleTheme = () => {
-  theme.global.name.value = isDarkTheme.value ? 'light' : 'dark'
-}
+  //
 </script>
--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <v-main>
+    <v-app-bar class="elevation-1" app>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon><v-icon>mdi-clipboard-multiple-outline</v-icon></v-app-bar-nav-icon>
+      </template>
+      <v-toolbar-title>Task Manager</v-toolbar-title>
+      <v-spacer />
+      <v-btn icon @click="toggleTheme">
+        <v-icon>{{ isDarkTheme ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+      </v-btn>
+    </v-app-bar>
     <router-view />
   </v-main>
 
@@ -7,5 +17,18 @@
 </template>
 
 <script setup>
-  //
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+const isDarkTheme = computed(() => theme.global.name.value === 'dark')
+
+const toggleTheme = () => {
+  theme.global.name.value = isDarkTheme.value ? 'light' : 'dark'
+}
 </script>
+
+<style>
+.elevation-1 {
+  box-shadow: 0 3px 8px rgba(34, 48, 62, 0.1), 0 0 transparent, 0 0 transparent !important;
+}
+</style>
